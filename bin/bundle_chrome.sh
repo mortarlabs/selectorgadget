75563df0193775b6d055bf86dffa731730cd64d1
Simon Mosk-Aoyama <simonsez@gmail.com>
2019-01-29 11:57:47 -0800

    fixed upload script

--- bin/bundle_chrome.sh
+++ bin/bundle_chrome.sh
@@ -4,3 +4,5 @@ cp build/selectorgadget_combined.css chrome/combined.css
 cat chrome/header.js build/selectorgadget_combined.min.js chrome/footer.js > chrome/combined.js
 rm -f extension.zip
 zip -r extension.zip chrome
+s3cmd put --no-check-md5 --cf-invalidate --acl-public -M --no-mime-magic extension.zip s3://saversage-selector-gadget
+
